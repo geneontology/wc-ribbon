d80ba0335394e74164c19e80604641d0fa145eac
Laurent-Philippe Albou <24249870+lpalbou@users.noreply.github.com>
2020-06-07 21:52:22 -0700

    New package for wc-go-ribbon but will use the new function in utils to simplify the integration for now

--- wc-go-ribbon/src/globals/utils.tsx
+++ wc-go-ribbon/src/globals/utils.tsx
@@ -1,21 +1,3 @@
-
-export function addEndingSlash(url) {
-    if(url == "") return url;
-    if(url.endsWith("/")) return url;
-    return url + "/";
-}
-
-/**
- * Transform http://example.com/page in page
- * @param url 
- */
-export function removeBaseURL(url) {
-    if(!url.startsWith("http://")) return url;
-    url = url.substring(7);
-    let murl = url.substring(url.indexOf("/") + 1);
-    return murl;
-}
-
 export function aspectShortLabel(txt) {
     if (txt == "biological_process") {
         return "P";
